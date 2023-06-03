b1519b3c9a9c8af1db9c83b767a7496b9e45de98
adityamaanas <64123559+adityamaanas@users.noreply.github.com>
2023-06-03 23:09:29 +0530

    Update diagnosis_RF.py

--- diagnosis_RF.py
+++ diagnosis_RF.py
@@ -1,19 +1,26 @@
 # Import the necessary libraries
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-
-# Load the data
-data = pd.read_csv('englishtraining.csv')
-#print(data.head())
-
-# Clean the data
-data = data.dropna()
-data = data.apply(lambda x: x.str.lower() if x.dtype == "object" else x)  # Normalize string columns to lowercase
+import wordembeddingsgensim as we
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(data, data['Disease'], test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(we.data['Embeddings'].values, we.data['Disease'].values, test_size=0.25)
+
+#print(X_train.shape)
+#print(y_train.shape)
+
+# Pad the sequences to the same length
+max_length = max(len(seq) for seq in X_train)
+X_train = pad_sequences(X_train, maxlen=max_length, padding='post')
+X_test = pad_sequences(X_test, maxlen=max_length, padding='post')
+
+# Flatten the sequences
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_test = X_test.reshape(X_test.shape[0], -1)
 
 # Train the model
 model = RandomForestClassifier(n_estimators=100, max_depth=10)
