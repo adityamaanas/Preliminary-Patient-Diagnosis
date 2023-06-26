647291cfa4a4568d047c3dc61d5d1504d1239247
Vishwajit <vishwajitjay2002@gmail.com>
2023-06-26 14:59:05 +0530

    RandomForest

--- RandomForest.py
+++ RandomForest.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error, r2_score
 
-
 data = pd.read_csv("C:/Users/vishw/OneDrive/Desktop/dataset.csv")
 df_padded = data.fillna(0)
 print(df_padded)
@@ -15,11 +14,12 @@ df_encoded = pd.get_dummies(df_padded)
 
 # Split the data into input features (X) and target variable (y)
 X = df_encoded.drop(df_encoded.columns[1], axis=1)  # Remove the target variable column if present
-print(X)
+#print("Hi \n",X)
 y = df_encoded[df_encoded.columns[1]]  # Set the target variable column
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#print("Hi \n", X_test)
 
 # Create and train the Random Forest Classifier
 model = RandomForestClassifier(n_estimators=3, max_depth=2, min_samples_leaf=50)
@@ -34,3 +34,8 @@ accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 #print("Mean Squared Error: ", mean_squared_error(y_test['x'], y_pred))
 #print("R2 Score: ", r2_score(y_test['x'], y_pred))
+
+input = X_test.iloc[0]
+print(input)
+prediction = bagging_classifier.predict([input])
+print(prediction)
