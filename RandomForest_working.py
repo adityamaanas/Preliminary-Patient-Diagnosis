17612ed630693e85a85e5813141c5e64ad3afd59
Aditya Maanas <adityamaanas2@gmail.com>
2023-06-26 19:48:33 +0530

    improved rf accuracy

--- RandomForest_working.py
+++ RandomForest_working.py
@@ -46,8 +46,7 @@ X_encoded = X_padded.replace(symptom_dict)
 X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
 
 # Create and train the Random Forest Classifier
-model = RandomForestClassifier(n_estimators=25, max_depth=2, min_samples_leaf=47, random_state=42)
-bagging_classifier = BaggingClassifier(estimator=model, n_estimators=30, random_state=42)
+bagging_classifier = RandomForestClassifier(n_estimators=300, max_depth=8, min_samples_leaf=110, random_state=42)
 bagging_classifier.fit(X_train, y_train)
 
 # Make predictions on the test set
